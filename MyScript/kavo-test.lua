930589589a760f3702dacc18f98c3c7966c56d97
S4ad0wKhab <11yvfgc@gmail.com>
2026-07-19 14:55:55 +0700

    Create kavo-test.lua

---
+++ MyScript/kavo-test.lua
@@ -0,0 +1,16 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+
+local Window = Library.CreateLib("TITLE", "DarkTheme")
+-- Themes:
+--    LightTheme
+--    DarkTheme
+--    GrapeTheme
+--    BloodTheme
+--    Ocean
+--    Midnight
+--    Sentinel
+--    Synapse
+
+local Tab = Window:NewTab("TabName") -- สร้างแท็บด้านซ้าย
+local Section = Tab:NewSection("Section Name") -- สร้างแท็บด้านขวา
+
