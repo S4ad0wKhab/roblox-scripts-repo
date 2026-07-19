7b638e070e512dac13b602798d078157948e30d9
S4ad0wKhab <11yvfgc@gmail.com>
2026-07-19 17:20:02 +0700

    Update mm2.lua

--- MyScript/mm2.lua
+++ MyScript/mm2.lua
@@ -5,12 +5,12 @@ local Window = OrionLib:MakeWindow({Name = "S4ad0wKhab's MM2 Script (Beta 1.0)",
 OrionLib:MakeNotification({
 	Name = "MM2 Script By S4ad0wKhab!",
 	Content = "Beta 1.0 is a test version; please let me know if you encounter any issues!",
-	Image = "rbxassetid://4483345998",
+	Image = "rbxassetid://116152473553878",
 	Time = 5
 })
 
 local Tab = Window:MakeTab({
 	Name = "Home",
-	Icon = "rbxassetid://4483345998",
+	Icon = "rbxassetid://77451122306832",
 	PremiumOnly = false
 })
