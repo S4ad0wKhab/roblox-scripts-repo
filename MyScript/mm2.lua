700652ab1d6979dafddd4ec2e5bf3ba169a5489b
S4ad0wKhab <11yvfgc@gmail.com>
2026-07-19 18:23:08 +0700

    Update mm2.lua

--- MyScript/mm2.lua
+++ MyScript/mm2.lua
@@ -1,4 +1,4 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/S4ad0wKhab/roblox-scripts-repo/refs/heads/main/library/polar-ui"))()
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/polar"))()
 
 local ok = library:CreateWindow("MM2 Script 1.0")
 
